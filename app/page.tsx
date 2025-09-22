2af60b3303b37e4849992494fce576596c06e0cc
Sayato <amf.kano@gmail.com>
2025-09-22 23:49:18 +0800

    feat: Update home page development status text

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
       <h1>TranSwitch</h1>
       <h2>This project is still under active development!</h2>
       <p>
-        See prototype <a href="/switch/0/view">here</a>.
+        Checkout the GitHub repo <a href="https://github.com/sayatodev/transwitch">here</a>.
+      </p>
+      <p>
+        Or check out the hidden page <a href="/switch/0/view">here</a>.
       </p>
     </div>
   );
