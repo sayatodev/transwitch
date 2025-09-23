94a5b8c945d2606c27ad4262974923bccf4e39d0
Sayato <amf.kano@gmail.com>
2025-09-23 09:01:20 +0800

    feat: Home page styling update

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
       <h1>TranSwitch</h1>
       <h2>This project is still under active development!</h2>
       <p>
-        Checkout the GitHub repo <a href="https://github.com/sayatodev/transwitch">here</a>.
+        Checkout the GitHub repo <a className="text-zinc-600" href="https://github.com/sayatodev/transwitch">here</a>.
       </p>
       <p>
-        Or check out the hidden page <a href="/switch/0/view">here</a>.
+        Or check out the hidden page <a className="text-zinc-600" href="/switch/0/view">here</a>.
       </p>
     </div>
   );
